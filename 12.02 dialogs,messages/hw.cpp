776b89e56a1d9265417342ed75402aacea5681f8
Марія Гребенчук <grebenchuk.mariia2004@outlook.com>
2024-02-26 07:29:47 +0200

    //

--- 12.02 dialogs,messages/hw.cpp
+++ 12.02 dialogs,messages/hw.cpp
@@ -5,7 +5,7 @@
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
 TCHAR str1[20];
-HWND mystatic;
+HWND mystatic[100]; // Массив для хранения хэндлов статиков
 HINSTANCE hInst1;
 int staticCount = 0; // Порядковые номера статиков(их количество)
 int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrev, LPTSTR lpszCmdLine, int nCmdShow)
@@ -46,11 +46,17 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT mes, WPARAM wp, LPARAM lp)
 
 	}
 	break;
+
+
+
+	//1)Пользователь «щелкает» левой кнопкой мыши по форме диалога и, не отпуская кнопку, ведёт по ней мышку,
+	//  а в момент отпускания кнопки по полученным координатам прямоугольника(как известно, двух точек на плоскости достаточно для создания прямоугольника)
+	//  создаётся «статик», который содержит свой порядковый номер(имеется в виду порядок появления «статика» на форме).
 	case WM_LBUTTONUP:
 	{
 		int width = LOWORD(lp) - x;
 		int height = HIWORD(lp) - y;
-		// Проверяем, что размеры статика не меньше 10х10
+		
 
 		//2)Минимальный размер «статика» составляет 10х10, а при попытке создания элемента управления меньших размеров
 		//  пользователь должен увидеть соответствующее предупреждение.
@@ -60,7 +66,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT mes, WPARAM wp, LPARAM lp)
 			break;
 		}
 		
-		mystatic = CreateWindowEx(0, TEXT("STATIC"), TEXT(""), WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER | WS_EX_CLIENTEDGE,
+		mystatic[staticCount] = CreateWindowEx(0, TEXT("STATIC"), TEXT(""), WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER | WS_EX_CLIENTEDGE,
 			x, y, width, height, hWnd, 0, hInst1, 0);
 		staticCount++;
 		TCHAR staticInfo[100];
@@ -69,16 +75,55 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT mes, WPARAM wp, LPARAM lp)
 	}
 
 	return FALSE;
+
+
+
+	//3)При щелчке правой кнопкой мыши над поверхностью «статика» в заголовке окна должна появиться информация о статике
+	// (порядковый номер «статика», ширина и высота, а также координаты «статика» относительно родительского окна).
+	// В случае если в точке щелчка находится несколько «статиков», TO предпочтение отдается «статику» наибольшим порядковым номером.
+	case WM_RBUTTONDOWN:
+	{
+	
+		int mouseX = LOWORD(lp);
+		int mouseY = HIWORD(lp);
+
+		for (int i = staticCount - 1; i >= 0; --i) {
+			RECT rc;
+			GetWindowRect(mystatic[i], &rc);
+			if (mouseX >= rc.left && mouseX <= rc.right && mouseY >= rc.top && mouseY <= rc.bottom) {
+				TCHAR staticInfo[100];
+				GetWindowText(mystatic[i], staticInfo, 100);
+				SetWindowText(hWnd, staticInfo);
+				break;
+			}
+		}
+	}
+	break;
+	//4)При двойном щелчке левой кнопки мыши над поверхностью << статика» он должен исчезнуть с формы(для этого можно воспользоваться функцией DestroyWindow,4
+	//  вызывая её для соответствующего объекта «статика»).В случае если в точке щелчка находится несколько «статиков», то предпочтение отдается «статику» с наименьшим порядковым номером.
+	case WM_LBUTTONDBLCLK:
+	{
+		int mouseX = LOWORD(lp);
+		int mouseY = HIWORD(lp);
+
+		
+		for (int i = 0; i < staticCount; ++i) {
+			RECT rc;
+			GetWindowRect(mystatic[i], &rc);
+			if (mouseX >= rc.left && mouseX <= rc.right && mouseY >= rc.top && mouseY <= rc.bottom) {
+				DestroyWindow(mystatic[i]);
+				
+				mystatic[i] = mystatic[staticCount - 1];
+				staticCount--;
+				break;
+			}
+		}
+	}
+	break;
+	}
+	return FALSE;
+
 }
 
 
 
-
-
-//
-//
-//1)Пользователь «щелкает» левой кнопкой мыши по форме диалога и, не отпуская кнопку, ведёт по ней мышку, а в момент отпускания кнопки по полученным координатам прямоугольника(как известно, двух точек на плоскости достаточно для создания прямоугольника) создаётся «статик», который содержит свой порядковый номер(имеется в виду порядок появления «статика» на форме).
-//3)При щелчке правой кнопкой мыши над поверхностью «статика» в заголовке окна должна появиться информация о статике(порядковый номер «статика», ширина и высота, а также координаты «статика» относительно родительского окна).В случае если в точке щелчка находится несколько «статиков», TO предпочтение отдается «статику» наибольшим порядковым номером.C
-//
-//4)При двойном щелчке левой кнопки мыши над поверхностью << статика» он должен исчезнуть с формы(для этого можно воспользоваться функцией DestroyWindow, вызывая её для соответствующего объекта «статика»).В случае если в точке щелчка находится несколько «статиков», то предпочтение отдается «статику» с наименьшим порядковым номером.
-//
